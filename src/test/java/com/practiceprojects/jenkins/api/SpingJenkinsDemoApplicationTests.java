31648c97fa29953aa48ee34a7832e0a317b51c92
Glen Sequeira <glensequeira8@gmail.com>
2022-06-27 22:17:45 -0500

    Project skeleton added

---
+++ src/test/java/com/practiceprojects/jenkins/api/SpingJenkinsDemoApplicationTests.java
@@ -0,0 +1,21 @@
+package com.practiceprojects.jenkins.api;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SpingJenkinsDemoApplicationTests {
+	private static Logger logger=LoggerFactory.getLogger(SpingJenkinsDemoApplicationTests.class);
+	@Test
+	void contextLoads() {
+		logger.info("Test Executed!");
+		assertEquals(true,true);
+	}
+
+}
